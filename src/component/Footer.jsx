35969072a863194cc28b5e6161eddaf470dea332
Gonzalo-Erceg <gonzalo.erceg@hotmail.com>
2022-01-03 17:21:05 -0300

    arreglando footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ import {Container} from "react-bootstrap"
 
 function Footer(){
     return(
-        <div style={{width:"100%", height:"8rem",marginTop:"100px",position:"absolute",bottom:"0"}} className="bg-dark mt-auto">
+        <div style={{width:"100%", height:"8rem",position:"absolute",bottom:"0"}} className="bg-dark mt-auto">
             <Container style={{color:"#fff"}} className="pt-2 d-flex flex-column align-items-center ">
                 <p>esta pagina es estatica, por lo tanto no almacena ni registra ningun dato que ingreses</p>
                 <span>App created by Gonzalo Erceg</span>
